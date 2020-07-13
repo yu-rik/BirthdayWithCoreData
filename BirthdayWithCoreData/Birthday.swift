02ab49ab1ec8fd9a0fda39e19319c2a711350169
Yurii Nosachenko <yurik_n@outlook.com>
2020-07-13 19:10:34 +0300

    Added icon,  viewControllers

---
+++ BirthdayWithCoreData/Birthday.swift
@@ -0,0 +1,21 @@
+//
+//  Birthday.swift
+//  BirthdayWithCoreData
+//
+//  Created by yurik on 7/13/20.
+//  Copyright © 2020 yurik. All rights reserved.
+//
+
+import Foundation
+class Birthday {
+    let firstName: String
+    let lastName: String
+    let birthDate: Date
+    
+    init(firstname: String, lastName: String, date: Date) {
+        self.firstName = firstname
+        self.lastName = lastName
+        self.birthDate = date
+    }
+    
+}
